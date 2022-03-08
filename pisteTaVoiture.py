9f045a7be21d75a94d7c1f6626eb946535aa8591
Zakaria Tobbal <49374657+Zakichanu@users.noreply.github.com>
2022-03-08 11:08:31 +0100

    Refactoring code

--- pisteTaVoiture.py
+++ pisteTaVoiture.py
@@ -10,18 +10,16 @@ cascadeAlgorithm = cv2.CascadeClassifier('classfierCascadeCar.xml')
 # Detecting cars
 while True:
     ret, frames = snippet.read()
-    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
     
     # Applying algorith to detect cars, using B&W filter with 1.1 as ScaleFactor and 9 neighbors minimum
-    cars = cascadeAlgorithm.detectMultiScale(gray, 1.1, 2)
+    cars = cascadeAlgorithm.detectMultiScale(frames, 1.15, 4)
 
     # Drawing rectangle to around cars
     for(coord_x, coord_y, width, height) in cars:
-        plates = frames[coord_y:coord_y + height, coord_x:coord_x + width]
-        cv2.rectangle(frames, (coord_x, coord_y), (coord_x+width, coord_y+height), (0, 0, 255), 2)
+        cv2.rectangle(frames, (coord_x, coord_y), (coord_x+width, coord_y+height), color=(0, 0, 255), thickness=2)
         cv2.putText(frames, 'Car', (coord_x, coord_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
 
-    frames = cv2.resize(frames,(600,400))
     cv2.imshow('Car Detection System', frames)
     k = cv2.waitKey(33)
     if k == 27:
